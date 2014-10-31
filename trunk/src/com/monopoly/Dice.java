e8c663b2faf181962b4f406ca1197998a59a0b63
ChuagsVO@gmail.com <ChuagsVO@gmail.com@b9028cf1-7a42-73c7-0f68-497ddd5e8191>
2014-10-31 12:12:39 +0000

    Initial commit

---
+++ trunk/src/com/monopoly/Dice.java
@@ -0,0 +1,19 @@
+package com.monopoly;
+
+public class Dice {
+    private int face;
+    
+    public Dice() {
+        this.face = 0;
+    }
+
+    public int getFace() {
+        return face;
+    }
+
+    public void setFace(int face) {
+        this.face = face;
+    }
+    
+    
+}

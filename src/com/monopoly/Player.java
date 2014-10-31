a51968fa4e0850101232c85ca7320490f780856c
robotmrv <robotmrv@gmail.com>
2014-10-31 14:23:39 +0000

    Add Java dummy classes and interfaces

---
+++ src/com/monopoly/Player.java
@@ -0,0 +1,42 @@
+package com.monopoly;
+
+/**
+ * Created by Roma on 31.10.2014.
+ */
+public class Player {
+    public void surrender() {
+
+    };
+
+    public boolean startTurn() {
+        return false;
+    }
+
+    public boolean finishTurn() {
+        return false;
+    }
+
+    public Money getMoney() {
+        return null;
+    }
+
+    public Property getProperty() {
+        return null;
+    }
+
+    public void move(int cellId) {
+
+    }
+
+    public int getPosition() {
+        return 0;
+    }
+
+    public Status getStatus() {
+        return null;
+    }
+
+    public void setStatus(Status status) {
+
+    }
+}

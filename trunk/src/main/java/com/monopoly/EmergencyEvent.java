b7c20e63a3b16f0cbae7fb591050db6e2e2208ed
robotmrv@gmail.com <robotmrv@gmail.com@b9028cf1-7a42-73c7-0f68-497ddd5e8191>
2014-11-06 17:47:39 +0000

    add: class EmergencyEvent rename: interface BuidingOps on BuildingOps

--- trunk/src/main/java/com/monopoly/EmergencyEvent.java
+++ trunk/src/main/java/com/monopoly/EmergencyEvent.java
@@ -10,19 +10,34 @@ import java.util.Random;
 * ЧПСобытие
 */
 public class EmergencyEvent implements Event {
-    private String name;
-    private String description;
-    private int cellCount;
-    Random random = new Random(cellCount);
-    public EmergencyEvent(String name, String description, int cellCount) {
+    private final String name;
+    private final String description;
+    private final int cellCount;
+    private final Board board;
+    Random random;
+    public EmergencyEvent(String name, String description, int cellCount, Board board) {
         this.name = name;
         this.description = description;
         this.cellCount = cellCount;
+        this.board = board;
+        random = new Random(cellCount);
     }
 
     @Override
     public void performEvent(List<Player> players) {
-
+        List<Cell> cells = board.getCells();
+        int randomIndex;
+        for (int i = 0; i < cellCount; i++) {
+            randomIndex = random.nextInt(cells.size());
+            Cell cell = board.getCells().get(randomIndex);
+            if (cell.getCellType() == CellType.PropertyCell) {
+                Property property = (Property) cell;
+                List<BuildingYard> buildingYards = property.getBuildingYards();
+                randomIndex = random.nextInt(buildingYards.size());
+                BuildingOps b = (BuildingOps) buildingYards.get(randomIndex).getBuilding();
+                b.levelDown();
+            }
+        }
     }
 
     @Override

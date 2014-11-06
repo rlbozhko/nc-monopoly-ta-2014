62d1c506a9fe25c2758d0ffb1e936eb7d3a5c359
robotmrv@gmail.com <robotmrv@gmail.com@b9028cf1-7a42-73c7-0f68-497ddd5e8191>
2014-11-06 12:45:05 +0000

    add classes CellType, Monopoly, PropertyCell, PropertyStatus modify Property, Cell

---
+++ trunk/src/main/java/com/monopoly/Monopoly.java
@@ -0,0 +1,15 @@
+package com.monopoly;
+
+/**
+ * Created by Roma on 06.11.2014.
+ */
+
+import java.util.List;
+
+/**
+ * Монополия
+ */
+public class Monopoly {
+    private String monopolyType;
+    private List<PropertyCell> MonopolyCells;
+}
---
+++ trunk/src/main/java/com/monopoly/PropertyStatus.java
@@ -0,0 +1,11 @@
+package com.monopoly;
+
+/**
+ * Created by Roma on 06.11.2014.
+ */
+/**
+ * СтатусСобственности
+ */
+public enum PropertyStatus {
+    Pledged, Unpledged
+}

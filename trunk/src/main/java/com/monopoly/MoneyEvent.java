0f861d79dbd218501376b37bdd7413a69572f1eb
ChuagsVO@gmail.com <ChuagsVO@gmail.com@b9028cf1-7a42-73c7-0f68-497ddd5e8191>
2014-11-07 15:04:32 +0000

    Added MoneyEvent

---
+++ trunk/src/main/java/com/monopoly/MoneyEvent.java
@@ -0,0 +1,31 @@
+package com.monopoly;
+
+import java.util.Random;
+
+import com.monopoly.tools.XORShiftRandom;
+
+public class MoneyEvent {
+	private final int MAX_VALUE_MONEY_FOR_MONEYEVENT = 200;
+	private String eventName;
+	private String descrition;
+
+	public MoneyEvent(String eventName, String descrition) {
+		this.eventName = eventName;
+		this.descrition = descrition;
+	}
+
+	Random random = new Random();
+	XORShiftRandom xorShiftRandom = new XORShiftRandom();
+	
+	public void doAction(Board board) {
+		Player player = board.currentPlayer();
+		boolean isAddMoney = random.nextBoolean();
+		if (isAddMoney) {
+			player.getWallet().addMoney(xorShiftRandom.nextInt(MAX_VALUE_MONEY_FOR_MONEYEVENT));
+		} else {
+			player.getWallet().subtractMoney(xorShiftRandom.nextInt(MAX_VALUE_MONEY_FOR_MONEYEVENT));;
+		}
+
+	}
+
+}

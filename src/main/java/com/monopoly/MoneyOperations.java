a08028e7d196e83a6d8f2871e685643df6972148
robotmrv <robotmrv@gmail.com>
2014-11-07 14:24:45 +0000

    by Maxim Dolina: class Player and interfaces

---
+++ src/main/java/com/monopoly/MoneyOperations.java
@@ -0,0 +1,13 @@
+package com.monopoly;
+
+import java.util.List;
+
+/**
+ * Created by mdolina on 07.11.2014.
+ */
+/**
+ * Деньги
+ */
+public interface MoneyOperations {
+    public Wallet getWallet();
+}

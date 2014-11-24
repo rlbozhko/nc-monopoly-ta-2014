fd36fe1184295adee84f1eb0c405e3494fc14bc4
ChuagsVO@gmail.com <ChuagsVO@gmail.com@b9028cf1-7a42-73c7-0f68-497ddd5e8191>
2014-11-24 11:55:53 +0000

    refactoring DiceGenerator, added junit 4.11

--- trunk/src/main/java/com/monopoly/board/dice/DiceGenerator.java
+++ trunk/src/main/java/com/monopoly/board/dice/DiceGenerator.java
@@ -6,19 +6,29 @@ public class DiceGenerator implements Runnable {
 	private final int MAX_VALUE_ON_FACE = 6;
 	private Dice die1;
 	private Dice die2;
+	private boolean flag;
 	private XORShiftRandom xorShiftRandom = new XORShiftRandom();
 
 	public DiceGenerator(Dice die1, Dice die2) {
 		this.die1 = die1;
 		this.die2 = die2;
 	}
+	
+	public synchronized boolean finish(){
+		this.flag = true;
+		return flag;
+	}
 
 	@Override
 	public void run() {
-		Thread.currentThread();
-		while (!Thread.interrupted()) {
+		while (!flag) {
 			die1.setFace(xorShiftRandom.nextInt(MAX_VALUE_ON_FACE));
 			die2.setFace(xorShiftRandom.nextInt(MAX_VALUE_ON_FACE));
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
---
+++ trunk/src/test/java/com/monopoly/DiceGeneratorTest.java
@@ -0,0 +1,70 @@
+package com.monopoly;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.monopoly.board.dice.Dice;
+import com.monopoly.board.dice.DiceGenerator;
+
+public class DiceGeneratorTest {
+	private DiceGenerator diceGenerator;
+	private Dice die1;
+	private Dice die2;
+
+	@Before
+	public void before() {
+		die1 = new Dice();
+		die2 = new Dice();
+		diceGenerator = new DiceGenerator(die1, die2);
+		Thread diceThred = new Thread(diceGenerator);
+		diceThred.start();
+
+	}
+	
+	@Test
+	public void generateValue() throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		Assert.assertTrue(die1.getFace() > 0 && die2.getFace() > 0);
+	}
+
+	@Test
+	public void generateValidValue() throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		boolean isValid = true;
+		for (int i = 0; i < 100; i++) {
+			if (die1.getFace() > 6 || die2.getFace() > 6) {
+				isValid = false;
+				break;
+			}
+			if (die1.getFace() == 0 || die2.getFace() == 0) {
+				isValid = false;
+				break;
+			}
+			TimeUnit.SECONDS.sleep(2);
+		}
+		Assert.assertTrue(isValid);
+	}
+	
+	@Test
+	public void finishThread() throws InterruptedException {
+		Dice die3 = new Dice();
+		Dice die4 = new Dice();
+		DiceGenerator diceGenerator2 = new DiceGenerator(die3, die4);
+		Thread diceThred2 = new Thread(diceGenerator2);
+		diceThred2.start();
+		TimeUnit.SECONDS.sleep(5);
+		diceGenerator.finish();
+		TimeUnit.SECONDS.sleep(2);
+		boolean isStop = true;
+		int temp = die3.getFace();
+		for (int i = 0; i < 10; i++) {
+			if (temp != die3.getFace()) {
+				isStop = false;
+			}
+		}
+		Assert.assertTrue(isStop);
+	}
+}

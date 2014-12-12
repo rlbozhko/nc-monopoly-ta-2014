6835aa33624374bed51571afb3fca884ae019ff0
ChuagsVO <ChuagsVO@gmail.com>
2014-12-12 14:00:06 +0000

    rafactoring XORShiftRandom, ValueGeneratorForDice

--- src/main/java/com/monopoly/board/dice/ValueGeneratorForDice.java
+++ src/main/java/com/monopoly/board/dice/ValueGeneratorForDice.java
@@ -13,34 +13,34 @@ public class ValueGeneratorForDice implements Runnable {
 	private int value1;
 	private int value2;
 	private boolean isGeneratorEnabled;
-	private RandomStrategy xorShiftRandomStrategy;
+	private RandomStrategy randomStrategy;
 	
-	public ValueGeneratorForDice (RandomStrategy xorShiftRandomStrategy) {
-		this.xorShiftRandomStrategy = xorShiftRandomStrategy;
+	public ValueGeneratorForDice (RandomStrategy randomStrategy) {
+		this.randomStrategy = randomStrategy;
 		this.value1 = 1;
 		this.value2 = 1;
 	}
 
-	public synchronized boolean finish(){
-		this.isGeneratorEnabled = true;
-		return isGeneratorEnabled;
+	public synchronized void finish(){
+		this.isGeneratorEnabled = false;
+		
 	}
 
 	@Override
 	public void run() {
-		while (!isGeneratorEnabled) {
-			value1 = xorShiftRandomStrategy.nextInt(MAX_VALUE_ON_FACE);
+		while (isGeneratorEnabled) {
+			value1 = randomStrategy.nextInt(MAX_VALUE_ON_FACE);
 				try {
 					TimeUnit.MILLISECONDS.sleep(GENERATE_SLEEP_TIME);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-			value2 = xorShiftRandomStrategy.nextInt(MAX_VALUE_ON_FACE);
+			value2 = randomStrategy.nextInt(MAX_VALUE_ON_FACE);
 		}
 	}
 	
-	public void changeRandomStrategy(RandomStrategy xorShiftRandomStrategy) {
-		this.xorShiftRandomStrategy = xorShiftRandomStrategy;
+	public void changeRandomStrategy(RandomStrategy strategy) {
+		this.randomStrategy = strategy;
 	}
 
 	public synchronized int getValue1() {

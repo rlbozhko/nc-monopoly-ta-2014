182052705c9149d7f2f4ab8073e0cc90eff2267d
robotmrv@gmail.com <robotmrv@gmail.com@b9028cf1-7a42-73c7-0f68-497ddd5e8191>
2015-02-16 15:10:53 +0000

    fix multiple BuyPropertyAction

--- trunk/src/main/java/com/monopoly/action/BuyPropertyAction.java
+++ trunk/src/main/java/com/monopoly/action/BuyPropertyAction.java
@@ -1,26 +1,46 @@
 package com.monopoly.action;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.monopoly.board.cells.PropertyCell;
 import com.monopoly.board.player.Player;
+import com.monopoly.board.player.PropertyManager;
+import com.monopoly.game.session.GameSession;
 import com.monopoly.io.IO;
 
 /**
  * Created by Roma on 28.11.2014.
  */
-public class BuyPropertyAction implements Action {
+
+public class BuyPropertyAction implements Action {	
 	public final static ActionType type = ActionType.BUY_PROPERTY;
+	private static Set<Player> buyers = new HashSet<Player>();
 	
     @Override
     public void performAction(Player player) {
-        PropertyCell property = (PropertyCell) player.getCurrentCell();
-        IO playerIO = ActionUtils.getPlayerIO(player);
-        if (playerIO.yesNoDialog("Хотите купить " + property.getName() + " за $" + property.getPrice() + "?")) {
+    	IO playerIO = ActionUtils.getPlayerIO(player);
+    	if (buyers.contains(player)) {
+    		playerIO.showWarning("Можно совершать только одну покупку одновременно");
+    		return;
+    	}
+    	
+    	buyers.add(player);
+    	
+        PropertyCell property = (PropertyCell) player.getCurrentCell();        
+        PropertyManager pm = GameSession.getInstance().getPropertyManager();
+        if (playerIO.yesNoDialog("Хотите купить " + property.getName() + " за $" + property.getPrice() + "?") 
+        		&& pm.getPropertyOwner(property) == null) {
             if (player.buyProperty(property)) {
                 ActionUtils.sendMessageToAll(player.getName() + " купил " + property.getName() + " за $" + property.getPrice());
             } else {
                 playerIO.showWarning("У Вас не достаточно средств");
             }
+        } else {
+        	playerIO.showWarning("Не удалось купить " + property.getName());
         }
+        
+        buyers.remove(player);
     }
 		
     @Override
